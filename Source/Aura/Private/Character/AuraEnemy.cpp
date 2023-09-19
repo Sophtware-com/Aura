6b24b9408604dd3013747ef659911382c640eca7
Michael Lange <michael@sophtware.com>
2023-09-19 17:30:29 -0400

    Added Character and Enemy classes

---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright 2023 by Sophtware
+
+
+#include "Character/AuraEnemy.h"
+
---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Copyright 2023 by Sophtware
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
